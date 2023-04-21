8275fd8fb18056795b7cba5ce5cba9e6bbb40703
Khoi Hoang <khoi.hoang@bouvet.no>
2023-04-21 20:26:07 +0200

    fix: 🐛 CORS issue

--- back-end/src/config/cors.ts
+++ back-end/src/config/cors.ts
@@ -1,1 +1,1 @@
-export const ALLOWED_ORIGINS: string[] = ['http://localhost:5173', 'http://localhost:5174'];
+export const ALLOWED_ORIGINS: string[] = ['http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173'];
